7f57f8fa442ce10087c82c56bde1d1a581c396b1
tomasz gandor <tomasz.gandor@gmail.com>
2011-02-21 23:05:01 +0100

    adding options to the profile displayer with help from the docs

--- show_profile.py
+++ show_profile.py
@@ -2,16 +2,35 @@
 
 import sys
 import pstats
+import os
 
 def main():
-    print sys.argv
-    if len(sys.argv) == 0:
-        print "Usage: %s profile_output [profile_output...]" % sys.argv[0]
+    if len(sys.argv) == 1:
+        print "\nUsage: %s profile_output [sort_order] [profile_output...]" % sys.argv[0]
+        print "Where sort order may be: time (default), cumulative, ..."
+        print """ (taken from documentation:)
+
+        Valid Arg                Meaning
+        'calls'         call count
+        'cumulative'    cumulative time
+        'file'          file name
+        'module'        file name
+        'pcalls'        primitive call count
+        'line'          line number
+        'name'          function name
+        'nfl'           name/file/line
+        'stdname'       standard name
+        'time'          internal time 
+"""
         exit()
     s = pstats.Stats(sys.argv[1])
+    order = 'time'
     for f in sys.argv[2:]:
-        s.add(f)
-    s.sort_stats('time').print_stats(20)
+        if os.path.exists(f):
+            s.add(f)
+        else:
+            order = f
+    s.sort_stats(order).print_stats(20)
     
 if __name__ == '__main__':
     main()
