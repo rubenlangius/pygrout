b4508ad3959e26f6b187a239830ba77adeee8889
tomasz gandor <tomasz.gandor@gmail.com>
2011-02-24 12:00:03 +0100

    show profile more configurable (number of rows)

--- show_profile.py
+++ show_profile.py
@@ -7,8 +7,9 @@ import os
 def main():
     if len(sys.argv) == 1:
         print "\nUsage: %s profile_output [sort_order] [profile_output...]" % sys.argv[0]
-        print "Where sort order may be: time (default), cumulative, ..."
-        print """ (taken from documentation:)
+        print """ 
+        Where sort order may be: time (default), cumulative, ...
+        (taken from documentation:)
 
         Valid Arg                Meaning
         'calls'         call count
@@ -23,14 +24,23 @@ def main():
         'time'          internal time 
 """
         exit()
-    s = pstats.Stats(sys.argv[1])
+
+    # the process
+
     order = 'time'
-    for f in sys.argv[2:]:
-        if os.path.exists(f):
-            s.add(f)
-        else:
-            order = f
-    s.sort_stats(order).print_stats(20)
+    files = filter(os.path.exists, sys.argv[1:])
+    
+    extra = list(set(sys.argv[1:])-files)
+    if len(extra) > 0:
+        order = extra[0]
+        if len(extra) > 1:
+            print "Warning: excess args ignored:", extra[1:]
+
+    s = pstats.Stats(files[0])
+    map(s.add, files[1:])
+
+    num_rows = int(os.getenv('ROWS', '20'))
+    s.sort_stats(order).print_stats(num_rows)
     
 if __name__ == '__main__':
     main()
