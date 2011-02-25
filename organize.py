26c153f5488f8a33623fdca8a309162c33b08a5a
tomasz gandor <tomasz.gandor@gmail.com>
2011-02-25 17:52:21 +0100

    organize.py - improved, added incremental: command: progress, grouping of results

--- organize.py
+++ organize.py
@@ -21,25 +21,20 @@ def read_as_set(f):
 def printf(set_):
     """Used to display a set, with count, sorted and textwrapped."""
     print "(%d)"%len(set_)
-    print textwrap.fill(" ".join(sorted(set_)))
+    un_derscore = lambda x: x.replace('_', '0')
+    print textwrap.fill(" ".join(sorted(set_, key=un_derscore)))
 
-def compare(*args):
-    """Read in the passed files and display differences."""
-    if len(args) < 2:
-        print "Provide at least two filenames to compare."
-        return
-    if len(args) > 2:
-        print "Warning: only 2 files work now."
-    first, secnd = map(read_as_set, args[:2])
-    print "Only in%s:" % args[0]
-    printf(first.difference(secnd))
-    print "Only in %s:" % args[1]
-    printf(secnd.difference(first))
-    print "In both:"
-    printf(first.intersection(secnd))    
+def print_grouped(sum_of_all):
+    """Output with printf, but Solomon and Homberger separately."""
     
-def union(*args):
-    """Read in the passed files and display the union (set sum)."""
+    # Junk suppressed:
+    """
+    print "All found results are:"
+    printf(sum_of_all)
+    print "Including junk:"
+    printf(sum_of_all.difference(
+        sel_solomons(sum_of_all), sel_homberger(sum_of_all)))
+    """
     
     # helpers (maybe later global)
     
@@ -51,21 +46,34 @@ def union(*args):
         """Select Solomon test names (only full 100 customer)."""
         return set(filter(re.compile('r?c?[0-9_]{5}$').match, set_))
     
-    if len(args) < 1:
-        print "Provide at least two filenames to add together."
-        return
-        
-    sets = map(read_as_set, args)
-    sum_of_all = set.union(*sets)
-    print "All found results are:"
-    printf(sum_of_all)
-    print "Including junk:"
-    printf(sum_of_all.difference(
-        sel_solomons(sum_of_all), sel_homberger(sum_of_all)))
     print "Full Solomon tests:"
     printf(sel_solomons(sum_of_all))
     print "Homberger tests:"
-    printf(sel_homberger(sum_of_all))
+    printf(sel_homberger(sum_of_all))    
+    
+def compare(*args):
+    """Read in the passed files and display differences."""
+    if len(args) < 2:
+        print "Provide at least two filenames to compare."
+        return
+    if len(args) > 2:
+        print "Warning: only 2 files work now."
+    first, secnd = map(read_as_set, args[:2])
+    print "Only in%s:" % args[0]
+    print_grouped(first.difference(secnd))
+    print "Only in %s:" % args[1]
+    print_grouped(secnd.difference(first))
+    print "In both:"
+    print_grouped(first.intersection(secnd))    
+    
+def union(*args):
+    """Read in the passed files and display the union (set sum)."""
+    if len(args) < 1:
+        print "Provide at least two filenames to add together."
+        return        
+    sets = map(read_as_set, args)
+    sum_of_all = set.union(*sets)
+    print_grouped(sum_of_all)
 
 def intersection(*args):
     """Set intersection of one (two) or more files."""
@@ -75,7 +83,19 @@ def intersection(*args):
     sets = map(read_as_set, args)
     product_of_all = set.intersection(*sets)
     print "The elements repeating all over again are:"
-    printf(product_of_all)
+    print_grouped(product_of_all)
+
+def progress(*args):
+    """Compare a list of files, displaying new items, not found before."""
+    sets = map(read_as_set, args)
+    total = set()
+    for arg, set_ in zip(args, sets):
+        if len(set_.difference(total)):
+            print "\n *** New things found in %s:" % arg
+            print_grouped(set_.difference(total))
+        else:
+            print "\n ... Nothing new in %s:" % arg
+        total.update(set_)
     
 def missing(*args):
     """List problem sets which are missing from all the arguments."""
@@ -88,6 +108,16 @@ def missing(*args):
             for s in ['_2','_4','_6','_8','10'] 
             for x in (['_%d' % i for i in xrange(1,10)]+['10']) ])
     
+    def gen_solomons():
+        """Set of all Solomon instance names."""
+        stats = [
+            ('c1', 9), ('c2', 8), 
+            ('r1', 12), ('r2', 11), 
+            ('rc1', 8), ('rc2', 8) ]
+        return set([ '%s%02d' % (fam, num)
+            for fam, count in stats
+            for num in xrange(1, count+1) ])
+        
     sum_of_all = set.union(*map(read_as_set, args))
     
     hombergers = gen_hombergers()
@@ -95,6 +125,14 @@ def missing(*args):
     difference = hombergers.difference(sum_of_all)
     if difference == hombergers:
         print "(ALL %d)" % len(hombergers)
+    else:
+        printf(difference)
+        
+    solomons = gen_solomons()
+    print "Missing Solomon tests:"
+    difference = solomons.difference(sum_of_all)
+    if difference == solomons:
+        print "(ALL %d)" % len(solomons)
     else:
         printf(difference)
 
@@ -156,23 +194,25 @@ def main():
     
     sets_good = set(cutoff.sub('', f.replace('100s/','')).lower()
                     for f in glob.glob("100s/*.p"))
-    sets_sometimes = sets_bad.intersection(sets_good)
+    ##sets_sometimes = sets_bad.intersection(sets_good)
     sets_always = sets_good.difference(sets_bad)
     sets_never = sets_bad.difference(sets_good)
     
     # print summaries (for every run)
         
     print "\nBad results:"
-    printf(sets_bad)
+    print_grouped(sets_bad)
     print "\nGood results:"
-    printf(sets_good)
+    print_grouped(sets_good)
+    # quieting down somewhat
+    """
     print "\nSolved sometimes:"
     printf(sets_sometimes)
     print "\nSolved never:"
     printf(sets_never)
     print "\nSolved always:"
     printf(sets_always)
-    
+    """
     # remove junk - medium solutions (conditionally)
     
     if len(present) > 2*len(sets_bad):
@@ -188,10 +228,11 @@ def main():
     
     create_file('never.txt', sets_never)
     create_file('bad.txt', sets_bad) # broadest
-    create_file('sometimes.txt', sets_sometimes)
-    create_file('100s/sometimes.txt', sets_sometimes)
+    ##create_file('sometimes.txt', sets_sometimes)
+    ##create_file('100s/sometimes.txt', sets_sometimes)
     create_file('100s/good.txt', sets_good) # broadest
     create_file('100s/always.txt', sets_always)
+    raw_input('Done. Press ENTER')
 
 # global list of functions
 from types import FunctionType
