b26986e4fdb898c07957898b907c6cbca4a4dcaa
tomasz.gandor <tomasz.gandor@localhost>
2011-07-29 00:59:44 +0200

    reorganizing helper

--- helper.py
+++ helper.py
@@ -15,15 +15,36 @@ from ui_helper import Ui_Helper
 
 class Helper(QtGui.QDialog):
     def __init__(self, parent=None):
+        # boilerplate
         QtGui.QDialog.__init__(self, parent)
         self.ui = Ui_Helper()
         self.ui.setupUi(self)
+        # add custom mpl canvas
+        self.fig = Figure(figsize=(600,600), dpi=72, facecolor=(1,1,1), edgecolor=(0,0,0))
+        self.ax = self.fig.add_subplot(111)
+        # plot something initially:
+        self.ax.plot([0,1])
+        self.canvas = FigureCanvas(self.fig)
+        self.ui.verticalLayout.addWidget(self.canvas)
+        # custom slots
+        QtCore.QObject.connect(self.ui.update, QtCore.SIGNAL("clicked()"), self.redraw)
+        # extra vars
+        self.lastmi = None
+        self.lastwait = None
 #        self.fileSystemModel = QtGui.QFileSystemModel()
 #        self.fileSystemModel.setRootPath('.')
 #        self.ui.treeView.setModel(self.fileSystemModel)
 #        self.ui.treeView.setRootIndex(self.fileSystemModel.index('./output'))
 #        self.target = None
 
+    def redraw(self):
+        from glob import glob
+        from pygrout import *
+        print dir()
+        for name in glob('solomons/*.txt'):
+            print "Should process", name
+        print "Not yet ready"
+    
     def accept(self):
 #        for idx in self.ui.treeView.selectedIndexes():
 #            print idx
@@ -38,15 +59,7 @@ class Helper(QtGui.QDialog):
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    
-    fig = Figure(figsize=(600,600), dpi=72, facecolor=(1,1,1), edgecolor=(0,0,0))
-    ax = fig.add_subplot(111)
-    ax.plot([0,1])
-    # generate the canvas to display the plot
-    canvas = FigureCanvas(fig)
-    
+    # almost standard:    
     helper = Helper()
-    helper.ui.verticalLayout.addWidget(canvas)
     helper.show()
-    
     sys.exit(app.exec_())
