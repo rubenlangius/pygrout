a029413a9fe50d568b28ea7a94361e1f59594c3e
tomasz.gandor <tomasz.gandor@localhost>
2011-07-29 00:04:34 +0200

    attempts on a gui for construction heuristics testing

--- helper.py
+++ helper.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python
 
-from PyQt4 import QtCore, QtGui
 import sys
 
-from ui_helper import Ui_Dialog
+import matplotlib
+matplotlib.use('Qt4Agg')
+import pylab
+
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+from PyQt4 import QtCore, QtGui
+from ui_helper import Ui_Helper
 
 class Helper(QtGui.QDialog):
     def __init__(self, parent=None):
         QtGui.QDialog.__init__(self, parent)
-        self.ui = Ui_Dialog()
+        self.ui = Ui_Helper()
         self.ui.setupUi(self)
-        self.fileSystemModel = QtGui.QFileSystemModel()
-        self.fileSystemModel.setRootPath('.')
-        self.ui.treeView.setModel(self.fileSystemModel)
-        self.ui.treeView.setRootIndex(self.fileSystemModel.index('./output'))
-        self.target = None
+#        self.fileSystemModel = QtGui.QFileSystemModel()
+#        self.fileSystemModel.setRootPath('.')
+#        self.ui.treeView.setModel(self.fileSystemModel)
+#        self.ui.treeView.setRootIndex(self.fileSystemModel.index('./output'))
+#        self.target = None
 
     def accept(self):
-        for idx in self.ui.treeView.selectedIndexes():
-            print idx
-            print self.fileSystemModel.filePath(idx)
-            print self.fileSystemModel.fileName(idx)
-            print '-'*10
+#        for idx in self.ui.treeView.selectedIndexes():
+#            print idx
+#            print self.fileSystemModel.filePath(idx)
+#            print self.fileSystemModel.fileName(idx)
+#            print '-'*10
         QtGui.QDialog.accept(self)
         
     def reject(self):
@@ -30,6 +38,15 @@ class Helper(QtGui.QDialog):
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
+    
+    fig = Figure(figsize=(600,600), dpi=72, facecolor=(1,1,1), edgecolor=(0,0,0))
+    ax = fig.add_subplot(111)
+    ax.plot([0,1])
+    # generate the canvas to display the plot
+    canvas = FigureCanvas(fig)
+    
     helper = Helper()
+    helper.ui.verticalLayout.addWidget(canvas)
     helper.show()
+    
     sys.exit(app.exec_())
--- ui_helper.py
+++ ui_helper.py
@@ -2,41 +2,101 @@
 
 # Form implementation generated from reading ui file 'helper.ui'
 #
-# Created: Sun May 29 22:25:44 2011
-#      by: PyQt4 UI code generator 4.7.4
+# Created: Fri Jul 29 00:03:43 2011
+#      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(797, 266)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_Helper(object):
+    def setupUi(self, Helper):
+        Helper.setObjectName(_fromUtf8("Helper"))
+        Helper.resize(661, 701)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
-        Dialog.setSizePolicy(sizePolicy)
-        self.formLayout = QtGui.QFormLayout(Dialog)
-        self.formLayout.setObjectName("formLayout")
-        self.verticalLayout = QtGui.QVBoxLayout()
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.formLayout.setLayout(1, QtGui.QFormLayout.LabelRole, self.verticalLayout)
-        self.treeView = QtGui.QTreeView(Dialog)
-        self.treeView.setObjectName("treeView")
-        self.formLayout.setWidget(2, QtGui.QFormLayout.FieldRole, self.treeView)
-        self.buttonBox = QtGui.QDialogButtonBox(Dialog)
-        self.buttonBox.setOrientation(QtCore.Qt.Vertical)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
-        self.formLayout.setWidget(1, QtGui.QFormLayout.FieldRole, self.buttonBox)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(Helper.sizePolicy().hasHeightForWidth())
+        Helper.setSizePolicy(sizePolicy)
+        self.verticalLayout = QtGui.QVBoxLayout(Helper)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.widget = QtGui.QWidget(Helper)
+        self.widget.setMinimumSize(QtCore.QSize(0, 0))
+        self.widget.setObjectName(_fromUtf8("widget"))
+        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.widget)
+        self.horizontalLayout_2.setMargin(0)
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.groupBox = QtGui.QGroupBox(self.widget)
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.horizontalLayout = QtGui.QHBoxLayout(self.groupBox)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.label = QtGui.QLabel(self.groupBox)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.horizontalLayout.addWidget(self.label)
+        self.mi = QtGui.QDoubleSpinBox(self.groupBox)
+        self.mi.setDecimals(2)
+        self.mi.setSingleStep(0.05)
+        self.mi.setProperty(_fromUtf8("value"), 1.0)
+        self.mi.setObjectName(_fromUtf8("mi"))
+        self.horizontalLayout.addWidget(self.mi)
+        self.has_waitlimit = QtGui.QCheckBox(self.groupBox)
+        self.has_waitlimit.setObjectName(_fromUtf8("has_waitlimit"))
+        self.horizontalLayout.addWidget(self.has_waitlimit)
+        self.waitlimit = QtGui.QSpinBox(self.groupBox)
+        self.waitlimit.setEnabled(False)
+        self.waitlimit.setMaximum(120)
+        self.waitlimit.setSingleStep(30)
+        self.waitlimit.setObjectName(_fromUtf8("waitlimit"))
+        self.horizontalLayout.addWidget(self.waitlimit)
+        self.horizontalLayout_2.addWidget(self.groupBox)
+        self.groupBox_2 = QtGui.QGroupBox(self.widget)
+        self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
+        self.horizontalLayout_3 = QtGui.QHBoxLayout(self.groupBox_2)
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.label_2 = QtGui.QLabel(self.groupBox_2)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.horizontalLayout_3.addWidget(self.label_2)
+        self.families = QtGui.QListWidget(self.groupBox_2)
+        self.families.setMinimumSize(QtCore.QSize(30, 80))
+        self.families.setObjectName(_fromUtf8("families"))
+        item = QtGui.QListWidgetItem(self.families)
+        item.setCheckState(QtCore.Qt.Checked)
+        item = QtGui.QListWidgetItem(self.families)
+        item.setCheckState(QtCore.Qt.Checked)
+        item = QtGui.QListWidgetItem(self.families)
+        item.setCheckState(QtCore.Qt.Checked)
+        self.horizontalLayout_3.addWidget(self.families)
+        self.label_3 = QtGui.QLabel(self.groupBox_2)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.horizontalLayout_3.addWidget(self.label_3)
+        self.horizontalLayout_2.addWidget(self.groupBox_2)
+        self.verticalLayout.addWidget(self.widget)
+        self.update = QtGui.QPushButton(Helper)
+        self.update.setObjectName(_fromUtf8("update"))
+        self.verticalLayout.addWidget(self.update)
 
-        self.retranslateUi(Dialog)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Dialog.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Dialog.reject)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(Helper)
+        QtCore.QObject.connect(self.has_waitlimit, QtCore.SIGNAL(_fromUtf8("toggled(bool)")), self.waitlimit.setEnabled)
+        QtCore.QMetaObject.connectSlotsByName(Helper)
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, Helper):
+        Helper.setWindowTitle(QtGui.QApplication.translate("Helper", "Route construction testing", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox.setTitle(QtGui.QApplication.translate("Helper", "Savings heuristic options", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Helper", "µ param", None, QtGui.QApplication.UnicodeUTF8))
+        self.has_waitlimit.setText(QtGui.QApplication.translate("Helper", "use waitlimit", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox_2.setTitle(QtGui.QApplication.translate("Helper", "Test group selection", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("Helper", "C / R / RC", None, QtGui.QApplication.UnicodeUTF8))
+        __sortingEnabled = self.families.isSortingEnabled()
+        self.families.setSortingEnabled(False)
+        self.families.item(0).setText(QtGui.QApplication.translate("Helper", "C", None, QtGui.QApplication.UnicodeUTF8))
+        self.families.item(1).setText(QtGui.QApplication.translate("Helper", "R", None, QtGui.QApplication.UnicodeUTF8))
+        self.families.item(2).setText(QtGui.QApplication.translate("Helper", "RC", None, QtGui.QApplication.UnicodeUTF8))
+        self.families.setSortingEnabled(__sortingEnabled)
+        self.label_3.setText(QtGui.QApplication.translate("Helper", "to be done...", None, QtGui.QApplication.UnicodeUTF8))
+        self.update.setText(QtGui.QApplication.translate("Helper", "Update results", None, QtGui.QApplication.UnicodeUTF8))
 
