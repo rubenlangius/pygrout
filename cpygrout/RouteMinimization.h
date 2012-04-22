308381e6e9d002d9b212a0104798fb2e0614176a
tomasz.gandor <tomasz.gandor@localhost>
2012-04-23 00:07:44 +0200

    a header for RNG - should be pluggable

--- cpygrout/RouteMinimization.h
+++ cpygrout/RouteMinimization.h
@@ -1,4 +1,5 @@
 #include "vrptw.h"
+#include "rng.h"
 
 namespace vrptw
 {
@@ -7,12 +8,24 @@ class RouteMinimization
 {
     Solution s;
     Problem *p;
+    std::vector<int> ejectionPool;
 public:
     RouteMinimization(Problem *p_) : p(p_) {}
     RouteMinimization(Problem &p_) : p(&p_) {}
+    bool removeRoute()
+    {
+        int toRemove = RANDINT(s.routes.size());
+        // TODO: init EP, loop etc.
+        return false;
+    }
     void execute()
     {
         all_customers_as_routes(*p, s);
+        bool success;
+        do
+        {
+            success = removeRoute();
+        } while (success);
     }
     Solution getSolution() { return s; }
 };
---
+++ cpygrout/rng.h
@@ -0,0 +1,8 @@
+#ifndef RNG_H
+#define RNG_H
+
+#include <stdlib.h>
+
+#define RANDINT(n) (rand() % n)
+
+#endif
